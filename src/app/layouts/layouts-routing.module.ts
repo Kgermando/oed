a2bb2896afef9ee39e54944ee071b34279503180
Germain Kataku <katakugermain@gmail.com>
2020-05-14 20:42:48 +0100

    toolbar

--- src/app/layouts/layouts-routing.module.ts
+++ src/app/layouts/layouts-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutsComponent } from './layouts.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  { path: '', component: LayoutsComponent, children: []}
+  { path: '', component: LayoutsComponent, children: [
+    { path: 'home', component: HomeComponent },
+    
+    { path: '', redirectTo: 'home', pathMatch: 'full'}
+  ]}
 ];
 
 @NgModule({
--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutsRoutingModule } from './layouts-routing.module';
 import { LayoutsComponent } from './layouts.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [LayoutsComponent],
+  declarations: [LayoutsComponent, HomeComponent],
   imports: [
     CommonModule,
     LayoutsRoutingModule
